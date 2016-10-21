44838bd3b3d2e5323c35429db8233aacdfcfe384
feizheng <1290657123@qq.com>
2016-10-21 21:34:02 +0800

    intial

---
+++ index.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+BITNAMI_PATH='/opt/sites/yiqiping.net/';
+alias btna='$BITNAMI_PATH/ctlscript.sh';
+
+# btnami all
+alias bt-apache-start='btna start';
+alias bt-apache-stop='btna stop';
+alias bt-apache-restart='btna restart';
+
+# btnami apache
+alias bt-apache-start='btna start apache';
+alias bt-apache-stop='btna stop apache';
+alias bt-apache-restart='btna restart apache';
+
+# btnami mysql
+alias bt-mysql='$BITNAMI_PATH/mysql/bin/mysql';
+alias bt-mysql-start='btna start mysql';
+alias bt-mysql-stop='btna stop mysql';
+alias bt-mysql-restart='btna restart mysql';
